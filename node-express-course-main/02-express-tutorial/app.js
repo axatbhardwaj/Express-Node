f2d83d06d95422608807618fedc43b3c63ad5da5
axatbhardwaj <axatbhardwaj@outlook.com>
2023-08-01 17:00:16 +0530

    Express 1

--- node-express-course-main/02-express-tutorial/app.js
+++ node-express-course-main/02-express-tutorial/app.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log("user hit the server");
-    res.end("Hey ! Home Page");
+    res.end("Hey !, Home Page");
 })
 
 server.listen(5000, () => {
