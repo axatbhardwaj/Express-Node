a57677a6c41c736ce29bfa8337f783cb7f56c345
axatbhardwaj <axatbhardwaj@outlook.com>
2023-08-01 17:43:29 +0530

    Express 1.1

--- node-express-course-main/02-express-tutorial/app.js
+++ node-express-course-main/02-express-tutorial/app.js
@@ -1,9 +1,24 @@
 const http = require('http');
 //console.log('Express Tutorial')
-
 const server = http.createServer((req, res) => {
     console.log("user hit the server");
-    res.end("Hey !, Home Page");
+
+    const url = req.url;
+
+    if (url === '/') {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.write('<h1>home Page</h1>');
+        res.end();
+    }
+    else if (url === "/about") {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        res.write('<h1>About Page</h1>');
+        res.end();
+    }
+    else {
+        res.writeHead(400, { 'content-type': 'text/html' })
+        res.write("<h1>ERROR</h1>")
+    }
 })
 
 server.listen(5000, () => {
