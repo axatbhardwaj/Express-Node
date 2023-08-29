6ed8c22b34f8f26e5d77a9e25aeb9360891f5c2a
axatbhardwaj <axatbhardwaj@outlook.com>
2023-08-29 17:29:02 +0530

    task manager CRUD complete

--- my-Proj/03-task-manager/starter/app.js
+++ my-Proj/03-task-manager/starter/app.js
@@ -1,1 +1,39 @@
-console.log('Task Manager App')
+// console.log('Task Manager App')
+const connectDB = require('./db/connect');
+const tasks = require('./routes/tasks')
+const express = require('express')
+const app = express()
+const port = 3000
+require('dotenv').config()
+
+//middleware
+
+app.use(express.json());
+
+app.use('/api/v1/tasks', tasks);
+
+app.get('/home', (req, res) => {
+    res.status(200).send('Task Manager App');
+})
+
+console.log(process.env.DB_URL);
+
+const start = async () => {
+    try {
+        await connectDB(process.env.DB_URL);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`)
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
+//api.get('/api/v1/tasks') - get all the tasks 
+//api.post('/api/v1/tasks') - create a new task
+//api.get('/api/v1/tasks/:id') - get a single task
+//api.patch('/api/v1/tasks/:id') - update a task
+//api.delete('/api/v1/tasks/:id') - delete a task
---
+++ my-Proj/03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,78 @@
+const Task = require('../models/Task')
+const getAllTasks = async (req, res) => {
+    try {
+        const task = await Task.find({})
+        res.status(200).json(task)
+    } catch (error) {
+        res.status(500).json({ msg: error.message })
+    }
+}
+
+const getTask = async (req, res) => {
+    try {
+        const { id: taskName } = req.params;
+        // console.log(taskID);
+        // console.log(id);
+        const task = await Task.findOne({ name: taskName });
+        if (!task) {
+            return res.status(404).json({ msg: `Task not found with name : ${taskName} ` })
+        } else {
+            res.status(200).json({ task });
+        }
+    } catch (error) {
+        res.status(500).json({ msg: error.message })
+    }
+}
+
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body)
+        //res.send('create item ')
+        res.status(201).json(task);
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ msg: error.message })
+    }
+}
+
+
+const updateTask = async (req, res) => {
+    try {
+        const { id: taskName } = req.params;
+        const task = await Task.findOneAndUpdate({ name: taskName }, req.body)
+        if (!task) {
+            return res.status(404).json({ msg: `Task not found with name : ${taskName} ` })
+        }
+        return res.status(200).json({
+            Status: "Success",
+            Task: task,
+            msg: "Task Updated Successfully"
+        })
+    } catch (error) {
+        return res.status(500).json({ msg: error.message })
+    }
+}
+
+const deleteTask = async (req, res) => {
+    // res.send('delete task')
+    try {
+        const { id: taskName } = req.params;
+        const task = await Task.findOneAndDelete({ name: taskName });
+        if (!task) {
+            return res.status(404).json({ msg: `Task not found with name : ${taskName} ` })
+        }
+        return res.status(200).json({
+            Status: "Success",
+            Task: task,
+            msg: "Task Deleted Successfully"
+        });
+    } catch (error) {
+        res.status(500).json({ msg: error.message })
+    }
+}
+
+
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask }
+
+
